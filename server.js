4fbec480b0744a41eac6c286f92e326cf3d56c84
eya-mejri <eyamejri456@gmail.com>
2025-02-06 02:13:17 +0100

    crud complet pour utilisateur

--- server.js
+++ server.js
@@ -25,3 +25,58 @@ app.post('/adduser', async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 });
+
+app.delete('/deleteuser/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await utilisateur.findByIdAndDelete(id);
+        if (!deletedUser) {
+            return res.status(404).json({ error: "Utilisateur non trouvé" });
+        }
+        res.status(200).json({ message: "Utilisateur supprimé avec succès", deletedUser });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.get('/getall', async (req, res) => {
+    try {
+        const users = await utilisateur.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.get('/getbyid/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await utilisateur.findById(id);
+        if (!user) {
+            return res.status(404).json({ error: "Utilisateur non trouvé" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+app.put('/putuser', async (req, res) => {
+    try {
+        const { Email } = req.body;  // Récupère l'email dans le corps de la requête
+        const dataToUpdate = req.body;  // Récupère les autres informations à mettre à jour
+
+        // Trouver un utilisateur par son email
+        const updatedUser = await utilisateur.findOneAndUpdate({ Email }, dataToUpdate, { new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: "Utilisateur non trouvé" });
+        }
+
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+
